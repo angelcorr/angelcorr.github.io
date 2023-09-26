34f84f85649c3605f3e11fababbadbd2141bc1c2
Angeles Rondon <angelesemilianaa@gmail.com>
2023-09-25 19:23:54 -0500

    Change main index HTLM file
    * Add js code, transitions and new classes to improve landing appearance
    * Add favicon logo

--- main.js
+++ main.js
@@ -9,3 +9,30 @@ window.addEventListener('scroll', () => {
     content.classList.remove('active');
   }
 })
+
+const contentAnimation = document.getElementById('info');
+
+contentAnimation.addEventListener('animationend', () => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => 
+        entry.target.classList.remove('set-visibility', entry.isIntersecting)
+      );
+    },
+    { root: null, threshold: 1 }
+  );
+  observer.observe(document.querySelector('.my-info'));
+  observer.observe(document.querySelector('.about-me'));
+})
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) =>
+      entry.target.classList.add('active-right', entry.isIntersecting),
+      entry.target.classList.remove('active-left', entry.isIntersecting)
+    );
+  },
+  { root: null, threshold: 0.01 }
+);
+observer.observe(document.querySelector('.angeles-asset'));
+observer.observe(document.querySelector('.angeles-description'));
