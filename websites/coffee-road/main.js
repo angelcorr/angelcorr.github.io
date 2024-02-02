5bd37e7b905f5ff35b964dc10b09f676696c20a1
Ángeles Rondón <angelesemilianaa@gmail.com>
2024-02-02 07:36:55 -0500

    Modify websites path
    * Replace it with portfolio word to be more
    consistent.

--- websites/coffee-road/main.js
+++
@@ -1,9 +0,0 @@
-function nav(){
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.main-nav');
-  burger.addEventListener('click', ()=>{
-    nav.classList.toggle('show')
-  })
-}
-
-document.addEventListener('DOMContentLoaded', nav);
--- websites/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('animationend', () => {
-  document.querySelector('.title').classList.remove('set-visibility');
-});
-
-window.addEventListener('mousemove', () => {
-  document.querySelector('.content').classList.remove('transition-down')
-  document.querySelector('.content').classList.add('transition-up')
-});
